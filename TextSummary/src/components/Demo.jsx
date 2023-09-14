2a8ed2ac558d43cca4f514e2f397ee586608f8f3
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-09-14 17:42:11 -0400

    in progress

--- TextSummary/src/components/Demo.jsx
+++ TextSummary/src/components/Demo.jsx
@@ -1,8 +1,40 @@
 // will be used to display the test
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const demo = () => {
-  return <div>demoi</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter URL"
+            value=""
+            onChange={() => {}}
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            // peer - it will be focused when clicked or hover over
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        {/* url histroy - which have been visited */}
+      </div>
+    </section>
+  );
 };
 
 export default demo;
