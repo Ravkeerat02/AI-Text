d6cc3f2a88a80e9271a0579eb13bdc494af73581
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-10-17 02:55:19 -0400

    small fixes

--- TextSummary/src/components/Hero.jsx
+++ TextSummary/src/components/Hero.jsx
@@ -7,17 +7,24 @@ const Hero = () => {
           onClick={() =>
             window.open("https://github.com/Ravkeerat02/AI-Text", "_blank")
           }
-          className="black_btn a flex items-center  "
+          className="black_btn a flex items-center"
         >
           <i className="fab fa-github"></i>
           <span>GitHub</span>
         </button>
       </nav>
 
+      <div className="bg-yellow-200 p-4 rounded-md mb-4">
+        <p className="text-yellow-800">
+          ⚠️ Warning: ScrapeNinja fails to extract data from this source due to
+          anti-scraping protection. Data may not be accessible for processing.
+        </p>
+      </div>
+
       <h1 className="head_text">Article Summarizer</h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+        that transforms lengthy articles into clear and concise summaries.
       </h2>
     </header>
   );
--- TextSummary/src/services/store.js
+++ TextSummary/src/services/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { articleApi } from "./article";
+
 export const store = configureStore({
-  // used to grab/get sonething spefici
-  //setting up store - saves information regardin gthe application
-  //   will only store this specific data
-  reducer: { [articleApi.reducerPath]: articleApi.reducer },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(articleApi.middleware),
-});
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
